f0c13393c34e3530787e5727c18ed0b718e6314c
Andyfish <andyfish@AndyfishdeMacBook-Air.local>
2017-04-26 22:17:21 +0800

    yht

--- app_express.js
+++ app_express.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello world!add');
+  res.send('Hello world!add by 我');
 });
 
 var server = app.listen(3000, function () {
